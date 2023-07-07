78b0faa04371ca31cb5fc89164a98be03b1eb792
0xAco <adrien.couetoux.pro@gmail.com>
2023-07-08 00:55:48 +0200

    initial commit - static css + popup wip

---
+++ src/app.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/* viewer count theater mode - "Playing Pokémon for x viewers" 
+  <p data-test-selector="stream-info-card-component__description" class="CoreText-sc-1txzju1-0 kLILeB">
+    Playing
+    <a data-a-target="player-info-game-name" class="ScCoreLink-sc-16kq0mq-0 cfiiLd tw-link"href="/directory/game/The%20Binding%20of%20Isaac%3A%20Repentance">
+      The Binding of Isaac: Repentance
+    </a>
+    for x viewers
+  </p>
+*/
+
+/* parse the upper element 
+const theaterText = document.querySelector('p[data-test-selector="stream-info-card-component__description"]');
+const theaterCount = theaterText.split('</a>');
+console.log('theaterText: ', theaterText, ' - theaterCount: ', theaterCount);
+alert('theaterText: ', theaterText, ' - theaterCount: ', theaterCount);
+*/
+// alert("extension ready");
