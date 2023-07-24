18323d2be87f3c8c918c5e7216f71b8a2b10d456
0xAco <adrien.couetoux.pro@gmail.com>
2023-07-24 22:19:44 +0200

    wip default value for popup

--- src/background.js
+++ src/background.js
@@ -1,33 +1,73 @@
 'use strict';
 console.log('background up');
 
+let isInjected;
 const injectedStyle = 'src/injected.css';
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+// check if style is injected
+async function isStyleInjected() {
+  try {
+    const response = await fetch(chrome.runtime.getURL(injectedStyle));
+    const cssurl = response.url.slice(52);
+    // 52 first caracters is the chrome-extension/[id] prefix
+    if (cssurl === injectedStyle) return true;
+  } catch {
+    console.error('not found: ', injectedStyle);
+    return false;
+  }
+}
+
+// retrieve an info from background.js
+function retrieveInfo(info) {
+  console.log('requestedInfo: ', info);
+  let res;
+  switch (info) {
+    case 'injectionStatus':
+      res = isInjected;
+      break;
+    default:
+      res = null;
+      break;
+  }
+  console.log('value: ', res);
+  return res;
+}
+
+// inject or remove CSS from the site
+function handleCSS(checked) {
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    const tabId = tabs[0].id;
+
+    if (checked) {
+      chrome.scripting.insertCSS({
+        target: { tabId },
+        files: [injectedStyle]
+      });
+    } else {
+      chrome.scripting.removeCSS({
+        target: { tabId },
+        files: [injectedStyle]
+      });
+    }
+
+    isInjected = checked;
+  })
+}
+
+// inject css when loaded
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
   if (changeInfo.status === "complete") {
     chrome.scripting.insertCSS({
-      target: { tabId: tabId },
+      target: { tabId },
       files: [injectedStyle]
     });
+    isStyleInjected();
   }
 });
 
+// handle messages
 chrome.runtime.onMessage.addListener(message => {
-  if (message.isActivated !== undefined) {
-    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-      const tabId = tabs[0].id;
-
-      if (message.isActivated) {
-        chrome.scripting.insertCSS({
-          target: { tabId },
-          files: [injectedStyle]
-        });
-      } else {
-        chrome.scripting.removeCSS({
-          target: { tabId },
-          files: [injectedStyle]
-        });
-      }
-    })
-  }
+  console.log(message);
+  if (message.hasOwnProperty('requestInfo')) return retrieveInfo(message.requestInfo);
+  if (message.hasOwnProperty('activate')) return handleCSS(message.activate);
 });
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,6 +1,17 @@
 'use strict';
 
-console.log('init popup');
+function sendSliderUpdate(status) {
+  chrome.runtime.sendMessage({ activate: status });
+}
+
+function checkSlider() {
+  alert('checking slider');
+  chrome.runtime.sendMessage({ requestInfo: 'injectionStatus' })
+  // TODO: récupérer la valeur pour l'envoyer dans le slider
+    .then(msg => { alert(msg); slider.checked = response.value; }) 
+    .catch(err => { alert('error with checkSlider: ', err) });
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   const slider = document.getElementById('slider');
 
@@ -8,8 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
   slider.addEventListener('change', () => {
     sendSliderUpdate(slider.checked);
   })
-});
 
-const sendSliderUpdate = (status) => {
-  chrome.runtime.sendMessage({ isActivated: status });
-}
+  // restore check status
+  checkSlider();
+});
